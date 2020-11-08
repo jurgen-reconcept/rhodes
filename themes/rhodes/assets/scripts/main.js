11e70eeb668c92d34f72308a78c93e1dd2ddc86a
Jürgen Visser <jurgen@reconcept.nl>
2020-11-08 13:43:05 +0100

    added more ad tracking

--- themes/rhodes/assets/scripts/main.js
+++ themes/rhodes/assets/scripts/main.js
@@ -1,5 +1,40 @@
-// import Live from "./live";
 
-// document.addEventListener("DOMContentLoaded", function (event) {
-//   new Live();
-// });
+function trackAdEvent(eventAction, eventLabel, eventValue){
+
+  if(!ga){
+    console.log('no ga');
+    return;
+  }
+
+  tracker = ga.getAll()[0];
+  if (tracker) {
+    tracker.send("event", "Ad", eventAction, eventLabel, eventValue);
+  }
+}
+
+function trackAdRedirectClick(eventLabel){
+  trackAdEvent('Click to redirect', eventLabel, 1)
+}
+
+function trackAdLandingArrived(eventLabel){
+    trackAdEvent('Arrived on Landing Page', eventLabel, 0)
+}
+
+function trackRedirect(eventLabel){
+    trackAdEvent('Redirect', eventLabel, 0)
+}
+
+function redirectToPage(eventLabel, url){
+  try {
+    trackRedirect(eventLabel);
+  } catch(e){}
+  
+  // timeout to allow GA to track
+  setTimeout(() => {
+    window.location.href = url;
+  }, 200)
+}
+
+window.trackAdLandingArrived = trackAdLandingArrived;
+window.trackAdRedirectClick = trackAdRedirectClick;
+window.redirectToPage = redirectToPage;
