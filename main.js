31cacfe2077068ced3fc6d70bf53f9e7d967fa30
Jürgen Visser <jurgen@reconcept.nl>
2020-11-08 13:43:24 +0100

    Publishing to gh-pages (publish.sh)

--- main.js
+++ main.js
@@ -1,2 +1,34 @@
 (() => {
+  // main.js
+  function trackAdEvent(eventAction, eventLabel, eventValue) {
+    if (!ga) {
+      console.log("no ga");
+      return;
+    }
+    tracker = ga.getAll()[0];
+    if (tracker) {
+      tracker.send("event", "Ad", eventAction, eventLabel, eventValue);
+    }
+  }
+  function trackAdRedirectClick(eventLabel) {
+    trackAdEvent("Click to redirect", eventLabel, 1);
+  }
+  function trackAdLandingArrived(eventLabel) {
+    trackAdEvent("Arrived on Landing Page", eventLabel, 0);
+  }
+  function trackRedirect(eventLabel) {
+    trackAdEvent("Redirect", eventLabel, 0);
+  }
+  function redirectToPage(eventLabel, url) {
+    try {
+      trackRedirect(eventLabel);
+    } catch (e) {
+    }
+    setTimeout(() => {
+      window.location.href = url;
+    }, 200);
+  }
+  window.trackAdLandingArrived = trackAdLandingArrived;
+  window.trackAdRedirectClick = trackAdRedirectClick;
+  window.redirectToPage = redirectToPage;
 })();
